f8be8c7321211f3f02a7b50ab3eb47af8ce5bd36
Roman <cikipuki10@gmail.com>
2023-11-20 18:09:17 +0200

    section popular + adaptation

--- js/script.js
+++ js/script.js
@@ -27,3 +27,24 @@ if (filter) {
         })
     })
 }
+
+
+
+const swiper = new Swiper('.popular-slider', {
+    spaceBetween: 20,
+    slidesPerView: 1,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        992: {
+            slidesPerView: 3, 
+        },
+        660: {
+            slidesPerView: 2, 
+        }
+    }
+
+  });
